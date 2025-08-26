9bd9e4949374e56abb7c2015761878202a723740
denisofflive <denisofflive@mail.ru>
2025-08-26 23:35:12 +0300

    Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from pydantic import BaseModel, EmailStr
-
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 # Импортируем запрос и ответ создания пользователя, модель данных пользователя
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
-
 
 # Модель для агрегации возвращаемых данных фикстурой function_user
 class UserFixture(BaseModel):
@@ -20,16 +20,17 @@ class UserFixture(BaseModel):
     def password(self) -> str:  # Быстрый доступ к password пользователя
         return self.request.password
 
+    def authentication_user(self) -> AuthenticationUserSchema:
+        return AuthenticationUserSchema(email=self.email, password=self.password)
+
 
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
     return get_authentication_client()
 
-
 @pytest.fixture
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
-
 
 # Фикстура для создания пользователя
 @pytest.fixture
@@ -38,3 +39,7 @@ def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response = public_users_client.create_user(request)
     return UserFixture(request=request, response=response)  # Возвращаем все нужные данные
+
+@pytest.fixture()
+def private_users_client(function_user:UserFixture) -> PrivateUsersClient:
+    return get_private_users_client(function_user.authentication_user())
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
+    GetUserResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -14,3 +15,30 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+        Проверяет соответствие данных двух объектов UserSchema.
+
+        :param actual: Фактические данные пользователя для проверки.
+        :param expected: Ожидаемые данные пользователя.
+        :raises AssertionError: Если хотя бы одно поле не совпадает.
+        """
+
+    assert_equal(actual.id, expected.id, name='id')
+    assert_equal(actual.email, expected.email, name='email')
+    assert_equal(actual.last_name, expected.last_name, name='last_name')
+    assert_equal(actual.first_name, expected.first_name, name='first_name')
+    assert_equal(actual.middle_name, expected.middle_name, name='middle_name')
+
+
+def assert_get_user_response(get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
+    """
+        Проверяет, что данные пользователя при GET- запросе совпадают с данными при создании этого пользователя.
+
+        :param get_user_response: Данные пользователя, полученные при запросе.
+        :param create_user_response: Данные пользователя, заданные при создании.
+        :raises AssertionError: Если данные пользователя не совпадают.
+        """
+
+    assert_user(get_user_response, create_user_response)
