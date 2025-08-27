df52192c553d62a14f85a4b986cf0ecb0d0d0f3f
denisofflive <denisofflive@mail.ru>
2025-08-27 11:06:28 +0300

    Практика написания автотестов с использованием фикстур Эталонное решение

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,5 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
-    GetUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema, \
+    UserSchema
 from tools.assertions.base import assert_equal
 
 
@@ -16,29 +16,31 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
 
+
 def assert_user(actual: UserSchema, expected: UserSchema):
     """
-        Проверяет соответствие данных двух объектов UserSchema.
+    Проверяет, что фактические данные пользователя соответствуют ожидаемым.
 
-        :param actual: Фактические данные пользователя для проверки.
-        :param expected: Ожидаемые данные пользователя.
-        :raises AssertionError: Если хотя бы одно поле не совпадает.
-        """
-
-    assert_equal(actual.id, expected.id, name='id')
-    assert_equal(actual.email, expected.email, name='email')
-    assert_equal(actual.last_name, expected.last_name, name='last_name')
-    assert_equal(actual.first_name, expected.first_name, name='first_name')
-    assert_equal(actual.middle_name, expected.middle_name, name='middle_name')
-
-
-def assert_get_user_response(get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
+    :param actual: Фактические данные пользователя.
+    :param expected: Ожидаемые данные пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-        Проверяет, что данные пользователя при GET- запросе совпадают с данными при создании этого пользователя.
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
 
-        :param get_user_response: Данные пользователя, полученные при запросе.
-        :param create_user_response: Данные пользователя, заданные при создании.
-        :raises AssertionError: Если данные пользователя не совпадают.
-        """
 
-    assert_user(get_user_response, create_user_response)
+def assert_get_user_response(
+        get_user_response: GetUserResponseSchema,
+        create_user_response: CreateUserResponseSchema
+):
+    """
+    Проверяет, что ответ на получение пользователя соответствует ответу на его создание.
+
+    :param get_user_response: Ответ API при запросе данных пользователя.
+    :param create_user_response: Ответ API при создании пользователя.
+    :raises AssertionError: Если данные пользователя не совпадают.
+    """
+    assert_user(get_user_response.user, create_user_response.user)
