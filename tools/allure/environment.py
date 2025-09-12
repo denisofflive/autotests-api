ae4b0ba16fbd0897a50e99b12f37be1924ad681d
denisofflive <denisofflive@mail.ru>
2025-09-12 12:25:42 +0300

    Практика работы с переменными окружения в Allure

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,9 +1,20 @@
+import platform
+import sys
+
 from config import settings
 
 
 def create_allure_environment_file():
+    os_info = f'{platform.system()}, {platform.release()}'
+    python_version = sys.version
     # Создаем список из элементов в формате {key}={value}
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+
+    items.extend([
+        f'os_info={os_info}',
+        f'python_version={python_version}'
+    ])
+
     # Собираем все элементы в единую строку с переносами
     properties = '\n'.join(items)
 
